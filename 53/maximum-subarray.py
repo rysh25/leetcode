3784032b1a9e1e2a231fca137a836f2b8949c90c
Ryohei Shiraki <rysh25@gmail.com>
2022-08-15 21:09:28 +0900

    first commit

---
+++ 53/maximum-subarray.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+# https://leetcode.com/problems/maximum-subarray/
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        ans = -float("inf")  # これまでの連続部分配列の和の最大値
+        tmp = 0  # 現在のインデックスを利用した連続部分配列の和の最大値
+        for i in range(len(nums)):
+            tmp = max(tmp + nums[i], nums[i])
+            ans = max(ans, tmp)
+
+        return ans
+
+
+sol = Solution()
+print(sol.maxSubArray(nums=[-2, 1, -3, 4, -1, 2, 1, -5, 4]))
