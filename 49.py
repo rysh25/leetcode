a44fed37a63629105f6f587743f166e4a4297c57
Ryohei Shiraki <rysh25@gmail.com>
2023-05-06 14:00:13 +0900

    Comment 49

--- 49.py
+++ 49.py
@@ -4,6 +4,15 @@ from typing import DefaultDict, List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        アナグラムグループを管理するためのディクショナリーを用意する
+        文字列でループしながら、ソートした文字列をキーとして、取得したリストに文字列を追加する
+        ループが終わった後、ディクショナリーないの全値をリストにして返却する。
+
+        Time complexity: O(m * n log n)
+            m=the size of strs, n=the average size of string in strs
+        Space complexixty: O(n)
+        """
         ans: DefaultDict[str, List[str]] = collections.defaultdict(list)
         for s in strs:
             sorted_wrod = "".join(sorted(s))
