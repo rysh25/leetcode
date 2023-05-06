00c4e1e6d79d2c6b2268a3a085de5ad582cfa536
Ryohei Shiraki <rysh25@gmail.com>
2023-05-06 13:45:40 +0900

    Comment 1

--- 1.py
+++ 1.py
@@ -4,6 +4,15 @@ from typing import DefaultDict, List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        nums 配列の各値を入れるディクショナリーを作る
+        nums 配列をループしながら、target から現在の値を引いた値がディクショナリーに
+        存在するか調べ、あれば、その組み合わせを返して終了。なければディクショナリーに値を設定し
+        次のループに進む
+
+        Time complexity: O(N)
+        Space complexity: O(N)
+        """
         ans: DefaultDict[int, int] = collections.defaultdict(int)
         for i, num in enumerate(nums):
             if target - num in ans:
