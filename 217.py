a4feea11269d1505fd2b911518cbe0fe5ddf29d9
Ryohei Shiraki <rysh25@gmail.com>
2023-05-06 13:28:21 +0900

    Comment 217

--- 217.py
+++ 217.py
@@ -3,6 +3,13 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        """
+        各数字が配列の中に2回以上出てくるか調べるために、nums を set に入れて、
+        長さが元と変わっているかを調べる。
+
+        Time complexity: O(N)
+        Space complexity: O(N)
+        """
         return len(nums) != len(set(nums))
 
 
