ac0532e89f9636a0af3ca19779195b9469f9e775
Ryohei Shiraki <rysh25@gmail.com>
2023-05-06 13:35:15 +0900

    Comment 242

--- 242.py
+++ 242.py
@@ -1,3 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        """
+        s と t がアナグラムか調べるため、お互いにソートし、違いがあるかを調べる
+
+        Time complexity: O(n long n)
+        Space complexity: O(n)
+        """
         return sorted(s) == sorted(t)
