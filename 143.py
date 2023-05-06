ea420db43bb38408dc22a36b28948d8f338033b2
Ryohei Shiraki <rysh25@gmail.com>
2023-05-06 13:23:09 +0900

    Reconsider 143

--- 143.py
+++ 143.py
@@ -8,50 +8,88 @@ class ListNode:
         self.next: Optional[ListNode] = next
 
 
+def print_linked_list(name: str, head: Optional[ListNode]):
+    print(f"{name}:")
+    if not head:
+        print()
+        return
+    curr = head
+    while curr:
+        print(curr.val, end=', ')
+        curr = curr.next
+    print()
+
+
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
+
+        Step 1. 真ん中を見つける
+        slow ポインター、fast ポインターを用意して head を入れる
+        slow を1づつ、fast を2づつ進めて、fast の next、next.next が存在するかぎり続ける
+        slow が真ん中を見る様になる （例えばリストのサイズが5・6なら slow は3番目まで進む)
+
+        Step 2. 後半を逆順にする
+        slow.next に None を入れる (slow を前半の最後にする)
+        curr に slow.next をいれる (これから slow の次を法はんの逆順の最後にする)
+        prev に None を入れる
+        curr が存在するかぎりループする:
+            curr を tmp に退避させる
+            curr を進める
+            tmp.next に prev を入れる
+            prev に tmp を入れる
+
+        prev に真ん中までの逆順が入る
+
+        Step 3. マージする
+        head1 に head、had2 に prev を入れる
+        head2 が存在するかぎりループする:
+            head1.next を tmp に退避させる
+            head1.next を head2 にする
+            head1 に head2 を入れる
+            head2 に tmp を入れる
+
+        Time complexity: O(N)
+        Space complexity: O(1)
         """
+
         if not head:
             return head
 
-        forward = head
-        prev = None
-        length = 0
-        while forward:
-            length += 1
-            prev = ListNode(forward.val, prev)
-            forward = forward.next
+        # Step 1. Find middle
+        slow, fast = head, head
 
-        tail = prev
+        while fast.next and fast.next.next:
+            if not slow:
+                Exception()
+            slow = slow.next
+            fast = fast.next.next
 
-        # print("tail: ")
-        # curr = tail
-        # while curr:
-        #     print(curr.val)
-        #     curr = curr.next
+        # Step 2. Reverse second half
+        if not slow:
+            Exception()
+            return
+        curr, prev = slow.next, None
+        slow.next = None
 
-        forward = head
-        backward = tail
+        # print_linked_list("head", head)
 
-        curr = forward
-        forward = forward.next
-
-        idx = 0
-        for i in range(length - 1):
-            if not curr or not forward or not backward:
-                break
-            if i % 2 == 0:
-                print(f"1: backward.val={backward.val}")
-                curr.next = backward
-                backward = backward.next
-            else:
-                print(f"2: forward.val={forward.val}")
-                curr.next = forward
-                forward = forward.next
+        while curr:
+            tmp = curr
             curr = curr.next
-        curr.next = None
+            tmp.next = prev
+            prev = tmp
+
+        # print_linked_list("prev", prev)
+
+        # Step 3. Merge two lists
+        head1, head2 = head, prev
+        while head2:
+            tmp = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = tmp
 
 
 sol = Solution()
@@ -62,9 +100,8 @@ n3 = ListNode(3, n4)
 n2 = ListNode(2, n3)
 list1 = ListNode(1, n2)
 
+print_linked_list("input", list1)
+
 sol.reorderList(list1)
 
-curr = list1
-while curr:
-    print(curr.val)
-    curr = curr.next
+print_linked_list("result", list1)
