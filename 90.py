327d6fc0e1b4a7192db0266e69237d4fb3c2e8a2
Ryohei Shiraki <rysh25@gmail.com>
2023-05-17 21:51:20 +0900

    Add 90

---
+++ 90.py
@@ -0,0 +1,45 @@
+class Solution:
+    def subsetsWithDup(self, nums: list[int]) -> list[list[int]]:
+        """
+        重複を含むの整数配列が指定されます。
+        すべての部分集合 (Subset) を返します。
+
+        バックトラックで再帰的に部分集合を作成します。
+
+        - Time complexity: O(2^n)
+        - Space complexity: O(n*2^n)
+
+        #BackTracking
+
+        Args:
+            nums (list[int]): 重複を含むの整数配列を指定します。
+
+        Returns:
+            list[list[int]]: すべての部分集合を返します。
+        """
+
+        ans: list[list[int]] = []
+
+        nums.sort()
+
+        def backtrack(i: int, curr: list[int]):
+            if i >= len(nums):
+                ans.append(curr[:])
+                return
+
+            curr.append(nums[i])
+            backtrack(i + 1, curr)
+
+            curr.pop()
+
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, curr)
+
+        backtrack(0, [])
+        return ans
+
+
+sol = Solution()
+print(sol.subsetsWithDup(nums=[1, 2, 2]))
+print(sol.subsetsWithDup(nums=[0]))
