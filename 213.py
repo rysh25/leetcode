c55d9dc0d627b68d84bff679c5c60f6a9b8e380b
Ryohei Shiraki <rysh25@gmail.com>
2023-09-29 00:53:26 +0900

    Add 213

--- 213.py
+++ 213.py
@@ -1,10 +1,13 @@
 class Solution:
     def rob(self, nums: list[int]) -> int:
         """
-        通り沿いの家に泥棒に入る。
+        円形に並んだ家に泥棒に入る。
         各家に隠してあるお金は、nums 配列で渡される。
         隣接する家に盗みに入ると自動的に警察に連絡される。
         警察に連絡されることなく、盗むことができる最大の金額を返す。
+
+        最後のの家を除いたもので DP したのち、最後の家をのじたもので DP する。
+        最後に、上記2つの結果の大きい方をとる。
 
         - Time complexity: O(n)
         - Space complexity: O(n)
@@ -17,6 +20,15 @@ class Solution:
         Returns:
             int: 盗むことができる最大の金額を返す。
         """
+        if len(nums) == 0:
+            return 0
+
+        if len(nums) == 1:
+            return nums[0]
+
+        return max(self.solve(nums[1:]), self.solve(nums[:-1]))
+
+    def solve(self, nums: list[int]) -> int:
         dp: list[int] = [0] * (len(nums) + 1)
 
         if len(nums) == 0:
@@ -31,6 +43,6 @@ class Solution:
 
 
 sol = Solution()
+print(sol.rob(nums=[2, 3, 2]))
 print(sol.rob(nums=[1, 2, 3, 1]))
-print(sol.rob(nums=[2, 7, 9, 3, 1]))
 print(sol.rob(nums=[1]))
