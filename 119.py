590780180ed33347d2ca4116f052e98f4b5267be
Ryohei Shiraki <rysh25@gmail.com>
2023-10-16 23:45:40 +0900

    Refine 119

--- 119.py
+++ 119.py
@@ -4,28 +4,24 @@ class Solution:
         整数 rowIndex が与えられ、0インデックスで、rowIndex 番目の
         パスカルの三角形の行を返す。
 
+        組み合わせの公式を使って求める。
+
+        - Time complexity: O(n)
+        - Time complexity: O(n)
+
         Args:
             rowIndex (int): 整数が与えられる。
 
         Returns:
             list[int]: rowIndex 番目のパスカルの三角形の行を返す。
         """
-        pascalsTriangle: list[int] = []
-        ans: list[int] = []
-        for r in range(0, rowIndex + 1):
-            # print(f"r={r}")
-            for c in range(r + 1):
-                # print(f"c={c}")
-                if c == 0 or c == r:
-                    pascalsTriangle.append(1)
-                else:
-                    pascalsTriangle.append(
-                        pascalsTriangle[-(r + 1)] + pascalsTriangle[-r]
-                    )
-                # print(f"{pascalsTriangle[-1]}", end=" ")
-                if r == rowIndex:
-                    ans.append(pascalsTriangle[-1])
-            # print()
+        ans: list[int] = [1]
+        prev = 1
+        for k in range(1, rowIndex + 1):
+            next = prev * (rowIndex - k + 1) // k
+            ans.append(next)
+            prev = next
+
         return ans
 
 
