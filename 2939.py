772253226bb7bc3866c20e18bb1b1fd789cc82ec
Ryohei Shiraki <rysh25@gmail.com>
2023-11-19 14:15:59 +0900

    Fix 2939

--- 2939.py
+++ 2939.py
@@ -13,8 +13,6 @@ class Solution:
         #     + format((a ^ x) * (b ^ x), f"0{n}b")
         #     + f" ({(a ^ x) * (b ^ x)})"
         # )
-        if n == 0:
-            return 0
 
         MOD = 10**9 + 7
 
@@ -22,23 +20,20 @@ class Solution:
             x = 0
             for i in range(n - 1, -1, -1):
                 bit = 1 << i
-                if i % 2 == 0:
-                    if (bit & a) == 0:
-                        x |= bit
-                else:
-                    if (bit & b) == 0:
-                        x |= bit
+                if (a ^ x) * (b ^ x) < (a ^ (x | bit)) * (b ^ (x | bit)):
+                    x |= bit
             # print("x=" + format(x, f"0{n}b") + f" ({x})")
             # print(f"max_product={((a ^ x) % MOD) * ((b ^ x) % MOD) % MOD}")
 
             return ((a ^ x) % MOD) * ((b ^ x) % MOD) % MOD
 
-        return max(max_product(a, b, n), max_product(b, a, n))
+        return max_product(a, b, n)
 
 
 sol = Solution()
-# print(sol.maximumXorProduct(a=12, b=5, n=4, x=2))
-# print(sol.maximumXorProduct(a=6, b=7, n=5, x=24))
-# print(sol.maximumXorProduct(a=1, b=6, n=3, x=5))
+print(sol.maximumXorProduct(a=12, b=5, n=4))
+print(sol.maximumXorProduct(a=6, b=7, n=5))
+print(sol.maximumXorProduct(a=1, b=6, n=3))
 print(sol.maximumXorProduct(a=0, b=4, n=0))
 print(sol.maximumXorProduct(a=0, b=10, n=7))  # 14875
+print(sol.maximumXorProduct(a=1, b=8, n=0))  # 8
